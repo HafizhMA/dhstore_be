5e5d97d56de798c2f13aa293961ec0e6c2c59c0d
hafizhma <muhammadazhr29@gmail.com>
2025-02-14 18:01:43 +0700

    install class validator and class transformer, create product dto

--- src/modules/product/dto/create-product.dto.ts
+++ src/modules/product/dto/create-product.dto.ts
@@ -1,1 +1,25 @@
-export class CreateProductDto {}
+import { IsBoolean, IsDefined, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateProductDto {
+    @IsString()
+    @IsDefined()
+    @IsNotEmpty()
+    name: string;
+
+    @IsNumber()
+    @IsDefined()
+    @IsNotEmpty()
+    price: number;
+
+    @IsNumber()
+    @IsOptional()
+    qty: number;
+
+    @IsBoolean()
+    @IsOptional()
+    isAdditional: boolean;
+
+    @IsBoolean()
+    @IsOptional()
+    active: boolean;
+}
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+import { InjectModel } from '@nestjs/sequelize';
 
 @Injectable()
 export class ProductService {
+  constructor (
+    @InjectModel(Product) private readonly productModel: typeof Product,
+  ){}
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
